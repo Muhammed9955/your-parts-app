00b3db789486281795b019c8fd6884bd757e6c40
Muhammed Khaled <muhammed.webdeveloper@gmail.com>
2025-02-19 14:07:48 +0200

    fix home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,46 +1,34 @@
 // app/[lang]/posts/page.tsx
 "use client";
 
-// import { useState } from "react";
+import { useState } from "react";
 // import Link from "next/link";
-// import { useAppwritePagination } from "@/hooks/usePosts";
+import { useAppwritePagination } from "@/hooks/usePosts";
 import Button from "@/components/ui/Button/Button";
-// import Pagination from "@/components/ui/Pagination/Pagination";
-// import PostList from "@/components/posts/PostList";
+import Pagination from "@/components/ui/Pagination/Pagination";
+import PostList from "@/components/posts/PostList";
 
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/routing";
-// import { Models } from "appwrite";
 
-// const ITEMS_PER_PAGE = 10;
-
-// interface AppwritePaginationResponse {
-//   total: number;
-//   documents: Models.Document[]; // Replace 'any' with your actual document type
-//   isLoading: boolean;
-//   isError: boolean;
-// }
+const ITEMS_PER_PAGE = 10;
 
 export default function PostsPage() {
   const t = useTranslations("home");
 
-  // const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  // const {
-  //   data: { total, documents },
-  //   isLoading,
-  //   isError,
-  // } = useAppwritePagination<AppwritePaginationResponse>(
-  //   currentPage,
-  //   ITEMS_PER_PAGE
-  // );
+  const { data, isLoading, isError } = useAppwritePagination(
+    currentPage,
+    ITEMS_PER_PAGE
+  );
   // console.log({ data });
-  // const totalPages = Math.ceil((total ?? 0) / ITEMS_PER_PAGE);
-  // // console.log({ totalPages });
-  // const handlePageChange = (newPage: number) => {
-  //   setCurrentPage(newPage);
-  //   window.scrollTo(0, 0);
-  // };
+  const totalPages = Math.ceil((data?.total ?? 0) / ITEMS_PER_PAGE);
+  console.log({ totalPages });
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+    window.scrollTo(0, 0);
+  };
 
   // const [page, setPage] = useState(1);
   // const limit = 10;
@@ -48,15 +36,15 @@ export default function PostsPage() {
   // // Fetch posts with React Query
   // const { data, isLoading, isError } = useGetPosts(page, limit);
   // console.log({ data });
-  // const posts = documents || [];
+  const posts = data?.documents || [];
 
-  // if (isLoading) {
-  //   return <div className="text-center py-10"> {t("loading")}</div>;
-  // }
+  if (isLoading) {
+    return <div className="text-center py-10"> {t("loading")}</div>;
+  }
 
-  // if (isError) {
-  //   return <div className="text-center py-10 text-red-500"> {t("error")} </div>;
-  // }
+  if (isError) {
+    return <div className="text-center py-10 text-red-500"> {t("error")} </div>;
+  }
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -69,13 +57,13 @@ export default function PostsPage() {
       </div>
 
       {/* <SearchBar /> */}
-      {/* <PostList posts={posts} />
+      <PostList posts={posts} />
 
       <Pagination
         currentPage={currentPage}
         totalPages={totalPages}
         onPageChange={handlePageChange}
-      /> */}
+      />
     </div>
   );
 }
